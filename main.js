2a3d6ef9259a04be800c69a8f2a7713580368715
Oliver Nguyen <olivern704@gmail.com>
2024-02-28 00:11:04 -0800

    mal api auth

---
+++ main.js
@@ -0,0 +1,175 @@
+
+const clientId = '7b20ca59e8a4f2a319c807d40fc8148c'
+const clientSecret = '67e1578cc37a6d30400a2f80c748fed4234dc3b5a0783fe30b236b87dff8b4cf'
+const redirectUrl = 'http://localhost:5173'
+
+const authorizationEndpoint ="https://myanimelist.net/v1/oauth2/authorize"
+const tokenEndpoint = "https://myanimelist.net/v1/oauth2/token"
+
+const currentToken = {
+  get access_token() { return localStorage.getItem('access_token') || null; },
+  get refresh_token() { return localStorage.getItem('refresh_token') || null; },
+  get expires_in() { return localStorage.getItem('refresh_in') || null },
+  get expires() { return localStorage.getItem('expires') || null },
+
+  save: function (response) {
+    const { access_token, refresh_token, expires_in } = response;
+    localStorage.setItem('access_token', access_token);
+    localStorage.setItem('refresh_token', refresh_token);
+    localStorage.setItem('expires_in', expires_in);
+
+    const now = new Date();
+    const expiry = new Date(now.getTime() + (expires_in * 1000));
+    localStorage.setItem('expires', expiry);
+  }
+};
+
+const args = new URLSearchParams(window.location.search);
+const code = args.get('code');
+
+if (code) {
+  const token = await getToken(code);
+  currentToken.save(token);
+
+  // Remove code from URL so we can refresh correctly.
+  const url = new URL(window.location.href);
+  url.searchParams.delete("code");
+
+  const updatedUrl = url.search ? url.href : url.href.replace('?', '');
+  window.history.replaceState({}, document.title, updatedUrl);
+}
+
+if (currentToken.access_token) {
+  const userData = await getUserData();
+  renderTemplate("main", "logged-in-template", userData);
+  renderTemplate("oauth", "oauth-template", currentToken);
+}
+
+// Otherwise we're not logged in, so render the login template
+if (!currentToken.access_token) {
+  renderTemplate("main", "login");
+}
+
+async function redirectToMALAuthorize() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < 100) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  
+
+
+  const code_verifier = result;
+
+  window.localStorage.setItem('code_verifier', code_verifier);
+
+  const authUrl = new URL(authorizationEndpoint)
+  const params = {
+    
+    client_id: clientId,
+    redirect_uri: redirectUrl,
+    code_challenge: code_verifier,
+    response_type: 'code',
+    code_challenge_method: 'plain',
+  };
+
+  authUrl.search = new URLSearchParams(params).toString();
+  window.location.href = authUrl.toString(); // Redirect the user to the authorization server for login
+}
+
+// MAL API Calls
+async function getToken(code) {
+  const code_verifier = localStorage.getItem('code_verifier');
+
+  const response = await fetch(tokenEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: clientId,
+      client_secret: clientSecret,
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirectUrl,
+      code_verifier: code_verifier,
+    }),
+  });
+
+  return await response.json();
+}
+
+async function refreshToken() {
+  const response = await fetch(tokenEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: currentToken.refresh_token
+    }),
+  });
+
+  return await response.json();
+}
+
+async function getUserData() {
+  const response = await fetch("", {
+    method: 'GET',
+    headers: { 'Authorization': 'Bearer ' + currentToken.access_token },
+  });
+
+  return await response.json();
+}
+
+// Click handlers
+async function loginWithMALClick() {
+  await redirectToMALAuthorize();
+}
+
+async function logoutClick() {
+  localStorage.clear();
+  window.location.href = redirectUrl;
+}
+
+async function refreshTokenClick() {
+  const token = await refreshToken();
+  currentToken.save(token);
+  renderTemplate("oauth", "oauth-template", currentToken);
+}
+
+// HTML Template Rendering with basic data binding - demoware only.
+function renderTemplate(targetId, templateId, data = null) {
+  const template = document.getElementById(templateId);
+  const clone = template.content.cloneNode(true);
+
+  const elements = clone.querySelectorAll("*");
+  elements.forEach(ele => {
+    const bindingAttrs = [...ele.attributes].filter(a => a.name.startsWith("data-bind"));
+
+    bindingAttrs.forEach(attr => {
+      const target = attr.name.replace(/data-bind-/, "").replace(/data-bind/, "");
+      const targetType = target.startsWith("onclick") ? "HANDLER" : "PROPERTY";
+      const targetProp = target === "" ? "innerHTML" : target;
+
+      const prefix = targetType === "PROPERTY" ? "data." : "";
+      const expression = prefix + attr.value.replace(/;\n\r\n/g, "");
+
+      // Maybe use a framework with more validation here ;)
+      try {
+        ele[targetProp] = targetType === "PROPERTY" ? eval(expression) : () => { eval(expression) };
+        ele.removeAttribute(attr.name);
+      } catch (ex) {
+        console.error(`Error binding ${expression} to ${targetProp}`, ex);
+      }
+    });
+  });
+
+  const target = document.getElementById(targetId);
+  target.innerHTML = "";
+  target.appendChild(clone);
+}
